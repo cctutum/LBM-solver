9c7a0a1110afe3344d4a457a0dff3874399beed3
Cem C. Tutum <cem.celal@gmail.com>
2024-10-02 23:35:47 -0500

    added cpu_intro folder fom LBM-book's repo

---
+++ cpp_cpu_intro/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -f sim
+
+CXXFLAGS="-std=c++98 -pedantic -O3 -Wall"
+
+g++ ${CXXFLAGS} seconds.cpp main.cpp -o sim -lrt
+
+# for mac:
+# g++ ${CXXFLAGS} seconds.cpp main.cpp -o sim
